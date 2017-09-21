4fb1820ed53d1e340564d5a72cd8da2f30fadfe6
Adam Söderström <adaso578@student.liu.se>
2017-09-21 10:57:54 +0200

    Lab3 initial commit

---
+++ Lab3/Server/src/Server.java
@@ -0,0 +1,18 @@
+
+public class Server {
+
+	private static final String PASSWORD = "123456";
+	private static final String KEYSTORE_PATH = "../SharedAssets/LIUkeystore.ks";
+	
+	
+	public Server(){
+		
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Hello server");
+	}
+
+}
